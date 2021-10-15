6fbf49e4e60525d4f12501652d2542e82925176d
Raphael dos Anjos <raphaanjos1@gmail.com>
2021-10-15 20:05:25 -0300

    Alteração nos atributos e notações

--- spring04/src/main/java/br/itau/spring04/controller/ContaController.java
+++ spring04/src/main/java/br/itau/spring04/controller/ContaController.java
@@ -1,5 +1,29 @@
 package br.itau.spring04.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.itau.spring04.model.Conta;
+import br.itau.spring04.repository.ContaRepo;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/")
 public class ContaController {
     
+    @Autowired // injeção de dependência
+    private ContaRepo repo;
+
+    @GetMapping("/contas")
+    public ResponseEntity<List<Conta>> listarContas() {
+        List<Conta> contas = (List<Conta>) repo.findAll();
+
+        return ResponseEntity.ok(contas);
+    }
 }
--- spring04/src/main/java/br/itau/spring04/model/Conta.java
+++ spring04/src/main/java/br/itau/spring04/model/Conta.java
@@ -13,53 +13,70 @@ public class Conta {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increment 1, 2, 3...
     @Column(name = "cod")
+    private int codConta;
+
+    @Column(name = "numero", nullable = false)
     private int numero;
 
-    @Column(name = "agencia", unique = true, nullable = false)
+    @Column(name = "agencia", nullable = false)
     private int agencia;
 
-    @Column(name = "tipo_conta", unique = true, nullable = false)
+    @Column(name = "tipo_conta", nullable = false)
     private int tipoConta;
 
     @Column(name = "saldo", nullable = false)
     private double saldo;
 
-    @Column(name = "titular", unique = true, nullable = false)
+    @Column(name = "titular", nullable = false)
     private String titular;
 
+   
+    public int getCodConta() {
+        return codConta;
+    }
 
+    public void setCodConta(int codConta) {
+        this.codConta = codConta;
+    }
 
     public int getNumero() {
         return numero;
     }
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
+
     public int getAgencia() {
         return agencia;
     }
+
     public void setAgencia(int agencia) {
         this.agencia = agencia;
     }
+
     public int getTipoConta() {
         return tipoConta;
     }
+
     public void setTipoConta(int tipoConta) {
         this.tipoConta = tipoConta;
     }
+
     public double getSaldo() {
         return saldo;
     }
+
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
+
     public String getTitular() {
         return titular;
     }
+
     public void setTitular(String titular) {
         this.titular = titular;
-    }
-
-    
+    }  
 
 }
