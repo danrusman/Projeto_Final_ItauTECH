43afac6c3362dff1c390fa6bd0a07da3faac576f
brunoSS07 <brunosss44@gmail.com>
2021-10-15 20:56:02 -0300

    atualizacao model

--- spring04/src/main/java/br/itau/spring04/model/Conta.java
+++ spring04/src/main/java/br/itau/spring04/model/Conta.java
@@ -24,7 +24,7 @@ public class Conta {
     @Column(name = "tipo_conta", nullable = false)
     private int tipoConta;
 
-    @Column(name = "saldo", nullable = false)
+    @Column(name = "saldo")
     private double saldo;
 
     @Column(name = "titular", nullable = false)
