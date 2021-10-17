0a9c785ae29863438875011da4c834a5a9ef066d
ramonvax <ramonvax@gmail.com>
2021-10-16 23:07:59 -0300

    modificado o ClienteRpo de classe para interface e feito o extrands para o CrudRepository

--- spring04/src/main/java/br/itau/spring04/repository/ClienteRepo.java
+++ spring04/src/main/java/br/itau/spring04/repository/ClienteRepo.java
@@ -2,7 +2,8 @@ package br.itau.spring04.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ClienteRepo {
+import br.itau.spring04.model.Cliente;
 
-    
+public interface ClienteRepo extends CrudRepository<Cliente, Long> {
+
 }
